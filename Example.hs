feb37e18241f23f39e4d7712307eb039da95df8c
Anton Ekblad <anton@ekblad.cc>
2016-12-02 16:18:51 +0100

    Add example of injecting EDSL terms into patterns

--- Example.hs
+++ Example.hs
@@ -7,6 +7,7 @@ readNum = do
     (new Nothing)
     (new $ Just (inj val))
 
+-- | Injecting EDSL terms into ADTs and binding values during matching
 test :: Stm ()
 test = void $ do
   mx <- readNum
@@ -14,6 +15,18 @@ test = void $ do
     [ Just 1       ~> (printS "one" >> pure true)
     , Nothing      ~> (printS "nope" >> pure true)
     , Just (var x) ~> (printN (val x) >> pure true)
+    ]
+
+-- | Injecting EDSL terms into patterns
+test2 :: Stm ()
+test2 = void $ do
+  printS "enter an argument to Right"
+  n <- scanN
+  x <- new $ (Right 42 :: Either Int Int)
+  match x
+    [ Right (inj n) ~> (printS "great, you guessed it" >> pure true)
+    , Right wc      ~> (printS "nope" >> pure true)
+    , wc            ~> (printS "something is seriously wrong" >> pure true)
     ]
 
 main = compileAndRun test
